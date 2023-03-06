9a752b701ec84f579126c3f51b5fc1c213e392d4
Neeraj Dharmadhikari <neerajd24@gmail.com>
2023-03-06 12:54:24 -0800

    Added a temporary view for the user to add items, prices, and people to split it with

--- app/App.js
+++ app/App.js
@@ -1,16 +1,168 @@
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Button, TextInput, Alert } from 'react-native';
 import NavBar from './components/NavBar';
-import LandingPage from './components/LandingPage';
+import LandingPage from './components/LandingPage'; 
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { Component, useState } from 'react';
+
 
 const backgroundImage = require('./assets/blue_purple_gradient.jpg');
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = ({navigation}) => {
   return (
-    <View style={styles.container}>
-      <ImageBackground source={backgroundImage} resizeMode="cover" style={styles.backgroundImage}>
+    <ImageBackground source={backgroundImage} resizeMode="cover" style={styles.backgroundImage}>
         <NavBar/>
         <LandingPage/>
-      </ImageBackground>
+        <Button
+          color={'white'}
+          title="Test app manually"
+          onPress={() =>
+            navigation.navigate('Reciept')
+          }
+        />
+    </ImageBackground>
+  );
+};
+
+class RecieptScreen extends Component {
+  state = {
+    numberOfItems: 3,
+    items: [],
+    prices: []
+  }
+
+  appendItem(item){
+    let newItems = this.state.items
+    newItems.push(item)
+    this.setState({items: newItems})
+    console.log(this.state.items)
+  }
+
+  appendPrice(price){
+    let newPrices = this.state.prices
+    console.log(this.state.items)
+    newPrices.push(price)
+    this.setState({prices: newPrices})
+    console.log(this.state.prices)
+  }
+
+  render(){
+    return (
+      <View>
+        <Text style={{height: 40, textAlign: 'center', paddingTop: 20, justifyContent: 'center',fontSize:15}}>Enter your reciept items and their prices</Text>
+
+        <View style={{flexDirection: 'row'}}>
+            <TextInput
+                style={{height: 40, textAlign: 'left', paddingLeft: 30, flex:3, fontSize:15}}
+                placeholder="Type the name of an item"
+                onEndEditing={(e) => this.appendItem(e.nativeEvent.text)}
+            />
+            <TextInput
+                style={{height: 40, textAlign: 'right', paddingRight: 30, flex:1,fontSize:15}}
+                placeholder="Cost"
+                onEndEditing={(e) => this.appendPrice(parseFloat(e.nativeEvent.text))}
+            />
+        </View>
+        <View style={{flexDirection: 'row'}}>
+            <TextInput
+                style={{height: 40, textAlign: 'left', paddingLeft: 30, flex:3, fontSize:15}}
+                placeholder="Type the name of an item"
+                onEndEditing={(e) => this.appendItem(e.nativeEvent.text)}
+            />
+            <TextInput
+                style={{height: 40, textAlign: 'right', paddingRight: 30, flex:1,fontSize:15}}
+                placeholder="Cost"
+                onEndEditing={(e) => this.appendPrice(parseFloat(e.nativeEvent.text))}
+            />
+        </View>
+        <View style={{flexDirection: 'row'}}>
+            <TextInput
+                style={{height: 40, textAlign: 'left', paddingLeft: 30, flex:3, fontSize:15}}
+                placeholder="Type the name of an item"
+                onEndEditing={(e) => this.appendItem(e.nativeEvent.text)}
+            />
+            <TextInput
+                style={{height: 40, textAlign: 'right', paddingRight: 30, flex:1,fontSize:15}}
+                placeholder="Cost"
+                onEndEditing={(e) => this.appendPrice(parseFloat(e.nativeEvent.text))}
+            />
+        </View>
+        <View style={{flexDirection: 'row'}}>
+            <TextInput
+                style={{height: 40, textAlign: 'left', paddingLeft: 30, flex:3, fontSize:15}}
+                placeholder="Enter the tax amount"
+                onEndEditing={(e) => this.appendItem(e.nativeEvent.text)}
+            />
+            <TextInput
+                style={{height: 40, textAlign: 'right', paddingRight: 30, flex:1,fontSize:15}}
+                placeholder="Tax"
+                onEndEditing={(e) => this.appendPrice(parseFloat(e.nativeEvent.text))}
+            />
+        </View>
+        <View style={{flexDirection: 'row'}}>
+            <TextInput
+                style={{height: 40, textAlign: 'left', paddingLeft: 30, flex:3, fontSize:15}}
+                placeholder="Enter the tip amount"
+                onEndEditing={(e) => this.appendItem(e.nativeEvent.text)}
+            />
+            <TextInput
+                style={{height: 40, textAlign: 'right', paddingRight: 30, flex:1,fontSize:15}}
+                placeholder="Tip"
+                onEndEditing={(e) => this.appendPrice(parseFloat(e.nativeEvent.text))}
+            />
+        </View>
+
+        <Text style={{height: 40, textAlign: 'center', paddingTop: 20, justifyContent: 'center',fontSize:15}}>Enter the names or initials of the everyone splitting</Text>
+        <View style={{flexDirection: 'row'}}>
+            <TextInput
+                style={{height: 40, textAlign: 'left', paddingLeft: 30, flex:3, fontSize:15}}
+                placeholder="Enter name"
+                onEndEditing={(e) => this.appendItem(e.nativeEvent.text)}
+            />
+        </View>
+        <View style={{flexDirection: 'row'}}>
+            <TextInput
+                style={{height: 40, textAlign: 'left', paddingLeft: 30, flex:3, fontSize:15}}
+                placeholder="Enter name"
+                onEndEditing={(e) => this.appendItem(e.nativeEvent.text)}
+            />
+        </View>
+        <View style={{flexDirection: 'row'}}>
+            <TextInput
+                style={{height: 40, textAlign: 'left', paddingLeft: 30, flex:3, fontSize:15}}
+                placeholder="Enter name"
+                onEndEditing={(e) => this.appendItem(e.nativeEvent.text)}
+            />
+        </View>
+        <Button
+          color={'green'}
+          title="Submit"
+          onPress={() =>
+            Alert.alert('Soon you can assign items!')
+          }
+        />
+      </View>)
+  }
+};
+
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Reciept" component={RecieptScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default function App() {
+
+  return (
+    <View style={styles.container}>
+      <MyStack />
     </View>
   );
 }
